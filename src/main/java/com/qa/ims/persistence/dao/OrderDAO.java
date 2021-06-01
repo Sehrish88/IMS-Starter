24ea56809bf224b48d38f31f0fffc1380d3e5224
Sehrish88 <sehrishbalouch4@gmail.com>
2021-06-01 15:29:22 -0400

    remove itemlist attribute and setter and getter from order domain

---
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -0,0 +1,108 @@
+package com.qa.ims.persistence.dao;
+import java.sql.ResultSet;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.persistence.domain.Order;
+import com.qa.ims.utils.DBUtils;
+
+
+
+public class OrderDAO implements Dao<Order> {
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
+		// TODO Auto-generated method stub
+		Long id = resultSet.getLong("id");
+		Long customer_id = resultSet.getLong("customer_id");
+		//List<Item> orderList = resultSet.getString("orderList"); 
+		return new Order(id, customer_id);
+	} 
+	
+	@Override
+	public List<Order> readAll() {
+		// TODO Auto-generated method stub
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");) {
+			List<Order> orders = new ArrayList<>();
+			while (resultSet.next()) {
+				orders.add(modelFromResultSet(resultSet));
+			}
+			return orders;
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		} 
+		return new ArrayList<>();
+		
+		
+	}
+
+	@Override
+	public Order read(Long id) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	public Order readLatest() {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet resultSet = statement.executeQuery("SELECT * FROM orders ORDER BY id DESC LIMIT 1");) {
+			resultSet.next();
+			return modelFromResultSet(resultSet);
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null;
+	}
+	
+	@Override
+	public Order create(Order order) {
+		// TODO Auto-generated method stub
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				Statement statement = connection.createStatement();) {
+			statement.executeUpdate("INSERT INTO orders(customer_id) VALUES(" 
+				+ order.getCustomer_id() + ")");  
+			return readLatest();
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		} 
+		return null;
+		
+		
+	}
+
+	@Override
+	public Order update(Order t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete(long id) {
+		// TODO Auto-generated method stub
+		
+		
+		return 0;
+	}
+
+
+	
+}
