e78c58d8e66d49fc719eff749bb8889e4f9e5dcb
Sehrish88 <sehrishbalouch4@gmail.com>
2021-06-03 15:04:09 -0400

    create order controller and add  CRUD methods

--- src/main/java/com/qa/ims/controller/OrderController.java
+++ src/main/java/com/qa/ims/controller/OrderController.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 
 import org.apache.logging.log4j.LogManager;
 import com.qa.ims.persistence.domain.Order;
-
+import com.qa.ims.persistence.domain.OrderItem;
 import com.qa.ims.persistence.dao.OrderDAO;
 import com.qa.ims.utils.Utils;
 
@@ -41,7 +41,7 @@ public class OrderController implements CrudController<Order>{
 		// TODO Auto-generated method stub
 		LOGGER.info("Please enter a customer id");
 		Long customer_id = utils.getLong();
-		Order order = orderDAO.create(new Order (customer_id)); 
+		Order order = orderDAO.create(new Order (customer_id));  
 		
 		
 		return order;
@@ -49,14 +49,64 @@ public class OrderController implements CrudController<Order>{
 
 	@Override
 	public Order update() {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub 
+		LOGGER.info("add or remove item");
+		String update = utils.getString().toLowerCase().trim(); 
+		
+		switch(update) {
+		case "add": 
+			addItem(); 
+			break; 
+		case "remove":
+			removeItem(); 
+			break;
+			
+		default:
+			update();  
+			
+		};
 		return null;
 	}
 
 	@Override
 	public int delete() {
 		// TODO Auto-generated method stub
-		return 0;
+		LOGGER.info("Please enter the id of the order you would like to delete");
+		Long id = utils.getLong();
+		LOGGER.info("Order deleted.");
+		return orderDAO.delete(id);
+	}
+	
+	
+	public OrderItem addItem() {
+	
+			LOGGER.info("Please enter a order id");
+			Long order_id = utils.getLong();
+			LOGGER.info("Please enter a item id");
+			Long item_id = utils.getLong();
+			LOGGER.info("Please enter a quantity");
+			Long quantity = utils.getLong();
+			OrderItem orderItem = orderDAO.addItem(new OrderItem(quantity, order_id, item_id)); 
+			// order_id && item_id -> order_item order = 1 item = 2
+			// join orders and order_item join order_item to your item
+			// does this id == this id
+			return orderItem; 
+		}
+	
+	
+	public int  removeItem() {
+		LOGGER.info("Please enter a order id");
+		Long order_id = utils.getLong();
+		
+		LOGGER.info("Please enter a item id of the item you want to remove");
+		Long item_id = utils.getLong();
+		
+		
+		return orderDAO.removeItem(order_id, item_id); 
+		//create a new read order item which will grab the entry from order-item table in orderDAO
+		//in order controller under remove item grab speific record which will give access to old quantity 
+		//do orderDAO.removeItem() passing in old quatity passing in specified quantity the order id and item id
+		
 	}
 
 } 
