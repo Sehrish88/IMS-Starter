953136b6dc8051a7a3cdbb837f28a1205e93b210
Sehrish88 <sehrishbalouch4@gmail.com>
2021-06-03 15:00:18 -0400

    create order-items table

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,4 +1,4 @@
-drop schema ims;
+
 
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
@@ -26,6 +26,23 @@ FOREIGN KEY(`fk_customer_id`) REFERENCES `customers`(`id`)
 
 ); 
 
+CREATE TABLE IF NOT EXISTS `ims`.`order-items`(
+`id` INT(11) NOT NULL AUTO_INCREMENT,
+`quantity` INT(11),
+`fk_order_id` INT(11) NOT NULL,
+`fk_item_id` INT(11) NOT NULL,
+PRIMARY KEY(`id`), 
+FOREIGN KEY(`fk_order_id`) REFERENCES `orders`(`id`),
+FOREIGN KEY(`fk_item_id`) REFERENCES `items`(`id`)
+); 
+
+use ims;
+SELECT * FROM `order-items`; 
+INSERT INTO orders(fk_customer_id) VALUES (1);
+SELECT * FROM customers; 
+INSERT INTO items(name, cost) VALUES ("Dress", 24.99);
+INSERT INTO `order-items`(`quantity`, `fk_order_id`, `fk_item_id`) VALUES (?, ?, ?);
+SELECT * FROM `order-items` oi JOIN `items` i ON oi.fk_item_id = i.id; 
 
 
 
