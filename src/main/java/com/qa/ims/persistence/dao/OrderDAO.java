aa5ae810f19b6eae07399424389c92b0cf85cc88
Sehrish88 <sehrishbalouch4@gmail.com>
2021-06-03 15:05:38 -0400

    create order DAO and build out CRUD functionality

--- src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/OrderDAO.java
@@ -15,6 +15,8 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.persistence.domain.Customer;
 import com.qa.ims.persistence.domain.Order;
 import com.qa.ims.utils.DBUtils;
+import com.qa.ims.persistence.domain.OrderItem;
+
 
 
 
@@ -25,8 +27,9 @@ public class OrderDAO implements Dao<Order> {
 	public Order modelFromResultSet(ResultSet resultSet) throws SQLException {
 		// TODO Auto-generated method stub
 		Long id = resultSet.getLong("id");
-		Long customer_id = resultSet.getLong("customer_id");
+		Long customer_id = resultSet.getLong("fk_customer_id");
 		//List<Item> orderList = resultSet.getString("orderList"); 
+		
 		return new Order(id, customer_id);
 	} 
 	
@@ -43,7 +46,7 @@ public class OrderDAO implements Dao<Order> {
 			return orders;
 		} catch (SQLException e) {
 			LOGGER.debug(e);
-			LOGGER.error(e.getMessage());
+			LOGGER.error(e.getMessage()); 
 		} 
 		return new ArrayList<>();
 		
@@ -84,9 +87,10 @@ public class OrderDAO implements Dao<Order> {
 	public Order create(Order order) {
 		// TODO Auto-generated method stub
 		try (Connection connection = DBUtils.getInstance().getConnection();
-				Statement statement = connection.createStatement();) {
-			statement.executeUpdate("INSERT INTO orders(customer_id) VALUES(" 
-				+ order.getCustomer_id() + ")");  
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO orders(fk_customer_id) VALUES (?)");) {
+			statement.setLong(1, order.getCustomer_id());
+			statement.executeUpdate();
 			return readLatest();
 		} catch (SQLException e) {
 			LOGGER.debug(e);
@@ -95,6 +99,22 @@ public class OrderDAO implements Dao<Order> {
 		return null;
 		
 		
+	}
+	
+	public OrderItem addItem(OrderItem orderItem) {
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection
+						.prepareStatement("INSERT INTO `order-items`(`quantity`, `fk_order_id`, `fk_item_id`) VALUES (?, ?, ?)");) {
+			statement.setLong(1, orderItem.getQuantity()); 
+			statement.setLong(2, orderItem.getOrder_id());
+			statement.setLong(3, orderItem.getItem_id());
+			statement.executeUpdate();
+			//return readLatest();
+		} catch (Exception e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return null; 
 	}
 
 	@Override
@@ -127,6 +147,18 @@ public class OrderDAO implements Dao<Order> {
 		return 0;
 	}
 
+	public int removeItem(Long orderId, Long itemId){
+		try (Connection connection = DBUtils.getInstance().getConnection();
+				PreparedStatement statement = connection.prepareStatement("DELETE FROM `order-items` WHERE `fk_order_id` = ? AND `fk_item_id` = ?");){
+			statement.setLong(1, orderId );
+			statement.setLong(2, itemId );
+			return statement.executeUpdate(); 
+		} catch (SQLException e) {
+			LOGGER.debug(e);
+			LOGGER.error(e.getMessage());
+		}
+		return 0;
+	}}
 
 	
-}
+
